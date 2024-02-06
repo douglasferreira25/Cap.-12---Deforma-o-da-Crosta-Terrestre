abb05c2eb5dd1d45170a963e988003239159ed36
douglasferreira25 <132702459+douglasferreira25@users.noreply.github.com>
2024-02-05 21:33:50 -0300

    Atualização do Arquivo de Entrada

--- arquivo_entrada.py
+++ arquivo_entrada.py
@@ -3,3 +3,10 @@ import fileinput
 for line in fileinput.input(files=('Dogbone_Tension.input')):
     # Process the input line
     print(line, end='')
+
+#-------------------------------------------------------------------------
+
+with open('FEM_Solver_Completo.py', 'r') as file:
+    content = file.read()
+    # Process the content of the input file
+    print(content)
